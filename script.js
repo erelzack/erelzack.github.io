3131e65ecd7f11a553eeb55a3107e5bc3fcf1aaf
erelzack <68661228+erelzack@users.noreply.github.com>
2026-06-29 03:15:59 +0300

    Add files via upload
    init. files

---
+++ script.js
@@ -0,0 +1,6 @@
+const helloButton = document.querySelector("#helloButton");
+const message = document.querySelector("#message");
+
+helloButton.addEventListener("click", () => {
+  message.textContent = "Your JavaScript is working!";
+});
