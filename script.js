622248fdb2f37d717a1bb3a42738960229040bfa
erelzack <erelzack@gmail.com>
2026-06-29 03:29:41 +0300

    changed script

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const helloButton = document.querySelector("#helloButton");
 const message = document.querySelector("#message");
 
 helloButton.addEventListener("click", () => {
-  message.textContent = "Your JavaScript is working!";
+  message.textContent = "Your JavaScript is working!!!";
 });
